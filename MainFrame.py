60d33902df87d627764114166a06e107ffb96e36
hugh <hugh_fly@hotmail.com>
2017-01-16 11:43:16 +1300

    Create Project

---
+++ MainFrame.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+class App(QMainWindow)
+    def __init__(self):
+        pass
+    def initUI(self):
+        pass
---
+++ SampleProject.py
@@ -0,0 +1,37 @@
+import sqlite3
+from skimage import io
+class Image():
+    def __init__(self):
+        pass
+
+    @property
+    def file_path(self):
+        pass
+
+    @property
+    def file_data(self):
+        pass
+
+    def load_img(self):
+        pass
+
+
+class ProjectSet():
+    def __init__(self):
+        pass
+
+    @property
+    def former(self):
+        pass
+
+    @property
+    def newer(self):
+        pass
+
+
+
+    def load_img(self):
+        pass
+
+    def exportDB(self):
+        pass
