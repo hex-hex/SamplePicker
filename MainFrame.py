8994cf0f7df62e60722fea2917bee69d9e4cb10d
hugh <hugh_fly@hotmail.com>
2017-01-16 11:45:49 +1300

    add project dialog

--- MainFrame.py
+++ MainFrame.py
@@ -2,6 +2,10 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
+class ProjectDialog():
+    def __init__(self):
+        pass
+
 class App(QMainWindow)
     def __init__(self):
         pass
